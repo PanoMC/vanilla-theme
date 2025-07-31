7f5c848fa70ec1114e7c72f1f2a89d7a4604ec5a
kahverengi <ahmet@duruer.dev>
2025-08-01 00:33:47 +0300

    fix: fix manifest is not added to zip

--- vite.config.js
+++ vite.config.js
@@ -34,12 +34,41 @@ function copyFolderPlugin(folder) {
   };
 }
 
+function copyManifestPlugin(filename = "manifest.json") {
+  let outDir = "";
+
+  return {
+    name: "copy-manifest-json",
+    apply: "build",
+    configResolved(config) {
+      outDir = config.build.outDir || "build";
+    },
+    async closeBundle() {
+      const srcPath = path.resolve(process.cwd(), filename);
+      const destPath = path.resolve(process.cwd(), outDir, filename);
+
+      if (!fs.existsSync(srcPath)) {
+        console.warn(`Manifest file not found at: ${srcPath}`);
+        return;
+      }
+
+      try {
+        await fs.promises.copyFile(srcPath, destPath);
+        console.log(`Copied manifest from ${srcPath} to ${destPath}`);
+      } catch (err) {
+        console.error("Failed to copy manifest.json:", err);
+      }
+    }
+  };
+}
+
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [
     sveltekit(),
     copyFolderPlugin("lang"),
-    copyFolderPlugin("screenshots")
+    copyFolderPlugin("screenshots"),
+    copyManifestPlugin()
   ],
   server: {
     proxy: {
