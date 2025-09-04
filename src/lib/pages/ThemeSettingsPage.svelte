5565955c92705ed6828348e58f0b358e1d5a3f8e
Selim Gökçek <selimgokcek@outlook.com>
2025-09-04 23:57:40 +0300

    add some preferences to ThemeSettingsPage

--- src/lib/pages/ThemeSettingsPage.svelte
+++ src/lib/pages/ThemeSettingsPage.svelte
@@ -1,22 +1,40 @@
 <!-- Tema ayarları -->
 <div class="card">
   <div class="card-header">
-    <ul class="nav nav-tabs card-header-tabs" id="themeSettingsTabs" role="tablist">
+    <ul
+      class="nav nav-tabs card-header-tabs"
+      id="themeSettingsTabs"
+      role="tablist">
       <li class="nav-item" role="presentation">
-        <button class="nav-link active" data-bs-target="#general" data-bs-toggle="tab"
-                id="general-tab" role="tab" type="button">
+        <button
+          class="nav-link active"
+          data-bs-target="#general"
+          data-bs-toggle="tab"
+          id="general-tab"
+          role="tab"
+          type="button">
           Genel
         </button>
       </li>
       <li class="nav-item" role="presentation">
-        <button class="nav-link" data-bs-target="#colors" data-bs-toggle="tab"
-                id="colors-tab" role="tab" type="button">
+        <button
+          class="nav-link"
+          data-bs-target="#colors"
+          data-bs-toggle="tab"
+          id="colors-tab"
+          role="tab"
+          type="button">
           Renkler
         </button>
       </li>
       <li class="nav-item" role="presentation">
-        <button class="nav-link" data-bs-target="#advanced" data-bs-toggle="tab"
-                id="advanced-tab" role="tab" type="button">
+        <button
+          class="nav-link"
+          data-bs-target="#advanced"
+          data-bs-toggle="tab"
+          id="advanced-tab"
+          role="tab"
+          type="button">
           Gelişmiş
         </button>
       </li>
@@ -26,13 +44,76 @@
     <div class="tab-content">
       <!-- Genel Ayarlar -->
       <div class="tab-pane fade show active" id="general" role="tabpanel">
-        <div class="mb-3">
-          <label class="form-label">Başlık</label>
-          <input class="form-control" placeholder="Site başlığını giriniz" type="text">
+        <div class="mb-3 form-floating">
+          <input class="form-control" type="text" />
+          <label class="form-label">Site Başlığı</label>
         </div>
         <div class="mb-3">
-          <label class="form-label">Logo</label>
-          <input class="form-control" type="file">
+          <label class="form-label">Logo Görünürlüğü</label>
+          <div class="form-check form-switch">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              id="flexSwitchCheckDefault" />
+          </div>
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Kapak Arkaplanı</label>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-check form-switch">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  id="flexSwitchCheckDefault" />
+              </div>
+            </div>
+            <div class="col-md-6">DRAG-DROP</div>
+          </div>
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Yan Panel</label>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-check form-switch">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  id="flexSwitchCheckDefault" />
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-floating">
+                <select class="form-select">
+                  <option value="1">Sağ</option>
+                  <option value="2">Sol</option>
+                </select>
+                <label for="">Yan Panel Konumu</label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Son Kayıt Olanlar</label>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-check form-switch">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  id="flexSwitchCheckDefault" />
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-floating">
+                <select class="form-select">
+                  <option value="1">Liste</option>
+                  <option value="2">Kafalar</option>
+                </select>
+                <label for="">Tablo Stili</label>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -40,19 +121,25 @@
       <div class="tab-pane fade" id="colors" role="tabpanel">
         <div class="mb-3">
           <label class="form-label">Ana Renk</label>
-          <input class="form-control form-control-color" type="color" value="#0d6efd">
+          <input
+            class="form-control form-control-color"
+            type="color"
+            value="#0d6efd" />
         </div>
         <div class="mb-3">
           <label class="form-label">Arka Plan Rengi</label>
-          <input class="form-control form-control-color" type="color" value="#ffffff">
+          <input
+            class="form-control form-control-color"
+            type="color"
+            value="#ffffff" />
         </div>
       </div>
 
       <!-- Gelişmiş Ayarlar -->
       <div class="tab-pane fade" id="advanced" role="tabpanel">
-        <div class="mb-3">
+        <div class="mb-3 form-floating">
+          <textarea class="form-control" style="height: 200px;"></textarea>
           <label class="form-label">Custom CSS</label>
-          <textarea class="form-control" placeholder="Ek CSS kodları buraya..." rows="4"></textarea>
         </div>
       </div>
     </div>
