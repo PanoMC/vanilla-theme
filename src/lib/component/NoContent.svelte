3733712d809da812912aca662c8d1338f48d0dfc
kahverengi <ahmet@duruer.dev>
2025-11-04 22:01:24 +0300

    fix: removed dark mode logic from no content component

--- src/lib/component/NoContent.svelte
+++ src/lib/component/NoContent.svelte
@@ -1,19 +1,12 @@
 <div class="container text-center">
-  {#if dark}
-    <i class="{icon} text-light text-opacity-25 m-3"></i>
-    <p class="text-light">{text}</p>
-  {:else}
-    <i class="{icon} text-dark text-opacity-25 m-3"></i>
-    <small class="text-gray d-block mb-3">{text}</small>
-  {/if}
-
+    <i class="{icon} text-opacity-25 m-3"></i>
+    <small class="d-block mb-3">{text}</small>
   <slot />
 </div>
 
 <script>
   import { _ } from "svelte-i18n";
 
-  export let dark = false;
   export let icon = "fa-solid fa-ghost fa-3x";
   export let text = $_("components.no-content.text");
 </script>
