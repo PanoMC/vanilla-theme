e31ac2b55d133bf5772419faa746b400e90cfbe3
kahverengi <ahmet@duruer.dev>
2025-11-13 18:38:54 +0300

    fix: fixed not getting correctly licenses.json

--- src/routes/(theme)/theme-api/licenses.json/+server.js
+++ src/routes/(theme)/theme-api/licenses.json/+server.js
@@ -3,12 +3,10 @@ import path from "path";
 
 /** @type {import("@sveltejs/kit").RequestHandler} */
 export async function GET() {
-  // First read from build folder
-  let filePath = path.resolve(process.cwd(), "build", "licenses.json");
+  let filePath = path.resolve(`licenses.json`);
 
-  // If not found in build folder, read from root directory (for development)
   if (!fs.existsSync(filePath)) {
-    filePath = path.resolve(process.cwd(), "licenses.json");
+    filePath = path.resolve(path.dirname(process.argv[1]) + `licenses.json`);
   }
 
   if (fs.existsSync(filePath)) {
