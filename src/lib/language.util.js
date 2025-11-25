3fda7911a13398c3f3fe13a7e5ed0b92b4c37cfe
kahverengi <ahmet@duruer.dev>
2025-11-25 12:45:19 +0300

    fix: fixed getAcceptedLanguage

--- src/lib/language.util.js
+++ src/lib/language.util.js
@@ -47,7 +47,10 @@ export async function init(initialLocale, event) {
 }
 
 export function getAcceptedLanguage(headers) {
-  if (typeof headers.get("accept-language") === "undefined") {
+  if (
+    typeof headers.get("accept-language") === "undefined" ||
+    headers.get("accept-language") == null
+  ) {
     return "";
   }
 
