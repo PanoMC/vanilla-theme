ae8f0b06b407edaf29d24808a35668366bc429fb
kahverengi <ahmet@duruer.dev>
2026-01-21 23:12:43 +0300

    fix: fixed extra main layout showing in error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,8 +1,5 @@
-<MainLayout>
-  <Error />
-</MainLayout>
+<Error />
 
 <script>
   import Error from "$lib/pages/ErrorPage.svelte";
-  import MainLayout from "$lib/layouts/MainLayout.svelte";
 </script>
