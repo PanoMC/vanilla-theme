acc71fd6d276c7c3836abeb6f73d9417d05ca12c
Selim Gökçek <selimgokcek@outlook.com>
2026-01-28 01:58:38 +0300

    style: Update toast component to use `text-bg-tertiary` and `shadow-lg` classes.

--- src/lib/component/Toast.svelte
+++ src/lib/component/Toast.svelte
@@ -1,6 +1,6 @@
 <div
   id="appToast{id}"
-  class="animate__animated animate__bounceInUp toast align-items-center bg-dark text-bg-dark shadow-sm"
+  class="animate__animated animate__bounceInUp toast align-items-center shadow-lg text-bg-tertiary"
   role="alert"
   aria-live="assertive"
   aria-atomic="true"
