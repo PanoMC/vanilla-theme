86b4b4140fc0d715f6e77527d376e4223d721013
Selim Gökçek <selimgokcek@outlook.com>
2026-01-30 21:15:34 +0300

    fix pageactions

--- src/lib/component/PageActions.svelte
+++ src/lib/component/PageActions.svelte
@@ -1,26 +1,39 @@
-<div class="row d-flex align-items-center gy-3">
-  {#if leftClasses !== null}
+<style>
+  /* Robust centering for scrollable flex containers */
+  .scroll-center::before,
+  .scroll-center::after {
+    content: "";
+    margin: auto;
+  }
+</style>
+
+<div
+  class="row d-flex align-items-center gy-3 justify-content-lg-between justify-content-center">
+  {#if (leftClasses !== null && $$slots.left) || (leftClasses !== null && leftClasses !== undefined)}
     <div
-      class="col-lg-4 col-12 d-flex justify-content-lg-start justify-content-center {leftClasses}">
+      class="col-lg-auto col-12 d-flex justify-content-lg-start overflow-x-auto scroll-center {leftClasses ||
+        ''}">
       <slot name="left" />
     </div>
   {/if}
-  {#if middleClasses !== null}
+  {#if (middleClasses !== null && $$slots.middle) || (middleClasses !== null && middleClasses !== undefined)}
     <div
-      class="col-lg-4 col-12 order-lg-2 order-last d-flex justify-content-center {middleClasses}">
+      class="col-lg-auto col-12 order-lg-2 order-last d-flex justify-content-center overflow-x-auto scroll-center {middleClasses ||
+        ''}">
       <slot name="middle" />
     </div>
   {/if}
-  {#if rightClasses !== null}
+  {#if (rightClasses !== null && $$slots.right) || (rightClasses !== null && rightClasses !== undefined)}
     <div
-      class="col-lg-4 col-12 order-3 d-flex justify-content-lg-end justify-content-center {rightClasses}">
+      class="col-lg-auto col-12 order-3 d-flex justify-content-lg-end overflow-x-auto scroll-center {rightClasses ||
+        ''}">
       <slot name="right" />
     </div>
   {/if}
 </div>
 
 <script>
-  export let leftClasses;
-  export let middleClasses;
-  export let rightClasses;
+  export let leftClasses = undefined;
+  export let middleClasses = undefined;
+  export let rightClasses = undefined;
 </script>
