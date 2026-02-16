b109e68a7a62b7994680b6c54669640926a8181c
Selim Gökçek <selimgokcek@outlook.com>
2026-02-16 22:46:22 +0300

    fix: update ActivateEmailPage view

--- src/lib/pages/ActivateEmailPage.svelte
+++ src/lib/pages/ActivateEmailPage.svelte
@@ -1,30 +1,37 @@
-<div class="col-lg-4 col-md-6 m-auto">
-  <div class="card">
-    <div class="card-body">
-      <h3 class="card-title text-center">{$_("pages.activate.title")}</h3>
-      <img
-        alt="Allay"
-        class="d-block mx-auto mb-4"
-        src="https://cdn3.emoji.gg/emojis/8182-allay-dancing.gif" />
-      <ErrorAlert error="{$error}" />
-      <SuccessAlert message="{$successMessage}" />
-      <button
-        class="btn btn-secondary w-100"
-        class:disabled="{$loading ||
-          $error === 'INVALID_LINK' ||
-          $successMessage !== null}"
-        disabled={$loading || $error === "INVALID_LINK" || $successMessage !== null}
-        on:click="{() => verifyEmail(error, successMessage, loading, data)}">
-        {#if $loading}
-          <span
-            class="spinner-border spinner-border-sm me-2"
-            role="status"
-            aria-label="Loading"></span>
-          <span>{$_("buttons.activate-email")}...</span>
-        {:else}
-          {$_("buttons.activate-email")}
-        {/if}
-      </button>
+<div class="col-lg-4 col-md-6 mx-auto">
+  <div class="vstack gap-3">
+    <PageTitle title={$_("pages.activate.title")} />
+
+    <div class="card">
+      <div class="card-body">
+        <div class="vstack gap-3">
+          <img
+            alt="Allay"
+            class="d-block mx-auto"
+            src="https://cdn3.emoji.gg/emojis/8182-allay-dancing.gif" />
+          <ErrorAlert error={$error} />
+          <SuccessAlert message={$successMessage} />
+          <button
+            class="btn btn-secondary w-100"
+            class:disabled={$loading ||
+              $error === "INVALID_LINK" ||
+              $successMessage !== null}
+            disabled={$loading ||
+              $error === "INVALID_LINK" ||
+              $successMessage !== null}
+            on:click={() => verifyEmail(error, successMessage, loading, data)}>
+            {#if $loading}
+              <span
+                class="spinner-border spinner-border-sm me-2"
+                role="status"
+                aria-label="Loading"></span>
+              <span>{$_("buttons.activate-email")}...</span>
+            {:else}
+              {$_("buttons.activate-email")}
+            {/if}
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </div>
@@ -44,6 +51,7 @@
   import { writable } from "svelte/store";
   import { _ } from "svelte-i18n";
 
+  import PageTitle from "$lib/components/PageTitle.svelte";
   import ErrorAlert from "$lib/components/ErrorAlert.svelte";
   import SuccessAlert from "$lib/components/SuccessAlert.svelte";
   import { verifyEmail } from "$lib/ui-logics/page-logics/ActiveEmailPageLogics.js";
--- src/lib/pages/ConfirmNewEmailPage.svelte
+++ src/lib/pages/ConfirmNewEmailPage.svelte
@@ -1,29 +1,37 @@
-<div class="col-lg-4 col-md-6 m-auto">
-  <div class="card">
-    <div class="card-body">
-      <h3 class="card-title">{$_("pages.activate-new-email.title")}</h3>
-      <img
-        alt="Allay"
-        src="https://cdn3.emoji.gg/emojis/8182-allay-dancing.gif" />
-      <ErrorAlert error="{$error}" />
-      <SuccessAlert message="{$successMessage}" />
-      <button
-        class="btn btn-secondary w-100"
-        class:disabled="{$loading ||
-          $error === 'INVALID_LINK' ||
-          $successMessage !== null}"
-        disabled={$loading || $error === "INVALID_LINK" || $successMessage !== null}
-        on:click="{() => verifyEmail(error, successMessage, loading, data)}">
-        {#if $loading}
-          <span
-            class="spinner-border spinner-border-sm me-2"
-            role="status"
-            aria-label="Loading"></span>
-          <span>{$_("buttons.activate-email")}...</span>
-        {:else}
-          {$_("buttons.activate-email")}
-        {/if}
-      </button>
+<div class="col-lg-4 col-md-6 mx-auto">
+  <div class="vstack gap-3">
+    <PageTitle title={$_("pages.activate-new-email.title")} />
+
+    <div class="card">
+      <div class="card-body">
+        <div class="vstack gap-3">
+          <img
+            alt="Allay"
+            class="d-block mx-auto"
+            src="https://cdn3.emoji.gg/emojis/8182-allay-dancing.gif" />
+          <ErrorAlert error={$error} />
+          <SuccessAlert message={$successMessage} />
+          <button
+            class="btn btn-secondary w-100"
+            class:disabled={$loading ||
+              $error === "INVALID_LINK" ||
+              $successMessage !== null}
+            disabled={$loading ||
+              $error === "INVALID_LINK" ||
+              $successMessage !== null}
+            on:click={() => verifyEmail(error, successMessage, loading, data)}>
+            {#if $loading}
+              <span
+                class="spinner-border spinner-border-sm me-2"
+                role="status"
+                aria-label="Loading"></span>
+              <span>{$_("buttons.activate-email")}...</span>
+            {:else}
+              {$_("buttons.activate-email")}
+            {/if}
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </div>
@@ -45,6 +53,7 @@
 
   import { verifyEmail } from "$lib/ui-logics/page-logics/ConfirmNewEmailPageLogics";
 
+  import PageTitle from "$lib/components/PageTitle.svelte";
   import ErrorAlert from "$lib/components/ErrorAlert.svelte";
   import SuccessAlert from "$lib/components/SuccessAlert.svelte";
 
